94457a19843e0f327619a7a13686ca408c3174ef
Akilah Powell <atpjun@gmail.com>
2022-12-01 20:03:19 -0400

    creating css styling for video games page

--- render-utils.js
+++ render-utils.js
@@ -2,6 +2,7 @@ export function renderVideoGames(gameObject) {
     const gameEl = document.createElement('div');
     const nameEl = document.createElement('h3');
     nameEl.textContent = gameObject.name;
+    gameEl.classList.add('gameEl');
     gameEl.append(nameEl);
 
     return gameEl;
