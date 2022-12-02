aeca15d3f71550063f2fafe1456c9332b8f7a876
Ashton Ruiz <ashton.ruiz@yahoo.com>
2022-12-01 17:14:30 -0800

    commented out render functions we just built.

--- render-utils.js
+++ render-utils.js
@@ -12,26 +12,24 @@ export function renderVideoGames(gameObject) {
     return gameEl;
 }
 
-export function renderCompetitors(competitorObject) {
-    const competitorEl = document.createElement('div');
-    const nameEl = document.createElement('h3');
+// export function renderCompetitors(competitorObject) {
+//     const competitorEl = document.createElement('div');
+//     const nameEl = document.createElement('h3');
 
+//     nameEl.textContent = competitorObject.name;
+//     competitorEl.classList.add('competitorEl');
+//     competitorEl.append(nameEl);
 
-    nameEl.textContent = competitorObject.name;
-    competitorEl.classList.add('competitorEl');
-    competitorEl.append(nameEl);
+//     return competitorEl;
+// }
 
-    return competitorEl;
-}
+// export function renderContact(contactObject) {
+//     const contactEl = document.createElement('div');
+//     const nameEl = document.createElement('h3');
 
-export function renderContact(contactObject) {
-    const contactEl = document.createElement('div');
-    const nameEl = document.createElement('h3');
+//     nameEl.textContent = contactObject.name;
+//     contactEl.classList.add('contactEl');
+//     contactEl.append(nameEl);
 
-
-    nameEl.textContent = contactObject.name;
-    contactEl.classList.add('contactEl');
-    contactEl.append(nameEl);
-
-    return contactEl;
-}
+//     return contactEl;
+// }
