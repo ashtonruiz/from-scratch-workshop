7c10ab36c6df2fb7b29e8c7850d9c37b863ec5ef
Akilah Powell <atpjun@gmail.com>
2022-12-01 21:01:59 -0400

    added render contact and competitor for formatting

--- render-utils.js
+++ render-utils.js
@@ -1,13 +1,37 @@
 export function renderVideoGames(gameObject) {
     const gameEl = document.createElement('div');
     const nameEl = document.createElement('h3');
-
     const img = document.createElement('img');
-    img.src = gameObject.id;
+
+    img.src = `./assets/${gameObject.id}.png`;
 
     nameEl.textContent = gameObject.name;
     gameEl.classList.add('gameEl');
-    gameEl.append(nameEl);
+    gameEl.append(nameEl, img);
 
     return gameEl;
+}
+
+export function renderCompetitors(competitorObject) {
+    const competitorEl = document.createElement('div');
+    const nameEl = document.createElement('h3');
+
+
+    nameEl.textContent = competitorObject.name;
+    competitorEl.classList.add('competitorEl');
+    competitorEl.append(nameEl);
+
+    return competitorEl;
+}
+
+export function renderContact(contactObject) {
+    const contactEl = document.createElement('div');
+    const nameEl = document.createElement('h3');
+
+
+    nameEl.textContent = contactObject.name;
+    contactEl.classList.add('contactEl');
+    contactEl.append(nameEl);
+
+    return contactEl;
 }
