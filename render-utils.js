28f334e911c7d2b84124d20fc8bed843bcf8080b
Akilah Powell <atpjun@gmail.com>
2022-12-01 20:40:36 -0400

    adding in pictures

--- render-utils.js
+++ render-utils.js
@@ -1,6 +1,10 @@
 export function renderVideoGames(gameObject) {
     const gameEl = document.createElement('div');
     const nameEl = document.createElement('h3');
+
+    const img = document.createElement('img');
+    img.src = gameObject.id;
+
     nameEl.textContent = gameObject.name;
     gameEl.classList.add('gameEl');
     gameEl.append(nameEl);
